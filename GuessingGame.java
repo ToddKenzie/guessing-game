7dad152b412ba4b8e8446beefc6626dc91f7fb61
Todd <mopeybear@gmail.com>
2018-08-26 12:02:17 -0400

    initial for statement

--- GuessingGame.java
+++ GuessingGame.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class GuessingGame {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int secretNumber = (int)(Math.random() * 10);
+		int secretNumber = (int) (Math.random() * 10);
+		int numGuesses = 4;
 //		System.out.println(secretNumber);
-		
+
 		System.out.println("Welcome to the Guessing Game!");
+		System.out.println("You will get " + numGuesses + " guesses.");
 		System.out.print("Pick a number between 1 - 10: ");
-		int guess = input.nextInt();
-		
-		if (secretNumber == guess) {
-			System.out.println("You have guessed the secret number!");
-		} else {
-			System.out.println("That's not it!");
+
+		boolean correctGuess = false;
+		for (int i = 1; i <= numGuesses; i++) {
+			int guess = input.nextInt();
+
+			if (secretNumber == guess) {
+				System.out.println("You have guessed the secret number!");
+			} else {
+				System.out.println("That's not it!");
+			}
+
 		}
-		
-		
-		
+
 		input.close();
 	}
 
