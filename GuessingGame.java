9417712f7e5248775e3229fc2baf771fd424a9b5
Todd <mopeybear@gmail.com>
2018-08-26 11:58:57 -0400

    add initial logic

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,8 +1,26 @@
 package guessingGame;
 
+import java.util.Scanner;
+
 public class GuessingGame {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int secretNumber = (int)(Math.random() * 10);
+//		System.out.println(secretNumber);
 		
+		System.out.println("Welcome to the Guessing Game!");
+		System.out.print("Pick a number between 1 - 10: ");
+		int guess = input.nextInt();
+		
+		if (secretNumber == guess) {
+			System.out.println("You have guessed the secret number!");
+		} else {
+			System.out.println("That's not it!");
+		}
+		
+		
+		
+		input.close();
 	}
 
 }
